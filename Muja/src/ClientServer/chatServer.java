6cda18f9caed983dae0a8d9f2d39c3538485931a
USER <USER@USER-PC>
2019-08-11 10:48:25 +0530

    client server connection

---
+++ Muja/src/ClientServer/chatServer.java
@@ -0,0 +1,43 @@
+package ClientServer;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class chatServer {
+	
+	public static void main(String[] args) {
+		
+		try {
+			ServerSocket ss = new ServerSocket(4443);
+			Socket s = ss.accept();
+			DataInputStream din = new DataInputStream(s.getInputStream());
+			DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			String str ="", str2 ="";
+			while(!str.equals("stop")) {
+				
+				str= din.readUTF();
+				System.out.println("Client says: "+str);
+				str2 =br.readLine();
+				dout.writeUTF(str2);
+				dout.flush();
+				
+			}
+			din.close();
+			s.close();
+			ss.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+
+}
